82b81b28642e1d50e26b75c3e8a2495beed4aca0
Marta <martamoralesfeito@gmail.com>
2019-05-19 23:50:25 +0200

    [Web] Home y Destinos

--- Web_Vuelos_1.0/js/destino.js
+++ Web_Vuelos_1.0/js/destino.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("header").load("/Web_Vuelos_1.0/layout/header.html");
-    $("footer").load("/Web_Vuelos_1.0/layout/footer.html")
+    $("footer").load("/Web_Vuelos_1.0/layout/footer.html");
 });
 
 
