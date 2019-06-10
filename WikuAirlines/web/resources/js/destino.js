16e372f665edd3bb89fd217d3ebaa515290daff4
Marta <martamoralesfeito@gmail.com>
2019-06-10 08:39:11 +0200

    des

--- WikuAirlines/web/resources/js/destino.js
+++ WikuAirlines/web/resources/js/destino.js
@@ -2,20 +2,3 @@ $(document).ready(function(){
     M.AutoInit();
 });
 
-
-/*Barra de navegacion*/
-function activo(link) {
-    var links = link.parentElement.getElementsByTagName("a");
-    for(var i of links){
-        i.classList.remove("active");
-    }
-    link.classList.add("active");
-}
-$('.pushpin-demo-nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-        top: $target.offset().top,
-        bottom: $target.offset().top + $target.outerHeight() - $this.height()
-    });
-});
