1ee71773e6ddb8eb56142b1f19c7925e719e4555
Marta <martamoralesfeito@gmail.com>
2019-06-11 17:50:45 +0200

    [Wiku] Css

---
+++ wiku/src/main/webapp/resources/js/menuUsuario.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    M.AutoInit();
+});
+
+/*Barra de navegacion*/
+function activo(link) {
+    var links = link.parentElement.getElementsByTagName("a");
+    for(var i of links){
+        i.classList.remove("active");
+    }
+    link.classList.add("active");
+}
+
