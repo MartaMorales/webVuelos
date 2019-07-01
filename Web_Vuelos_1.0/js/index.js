0f5d72db39fa31a0990d75f7455ffdfff99f2237
Marta <martamoralesfeito@gmail.com>
2019-07-01 19:11:25 +0200

    Limpia de repositorio

--- Web_Vuelos_1.0/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("header").load("/Web_Vuelos_1.0/layout/header.html");
-    $("footer").load("/Web_Vuelos_1.0/layout/footer.html")
-});
-
-
-
-
--- Web_Vuelos_1.0/js/login2.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $("footer").load("/Web_Vuelos_1.0/layout/footerlogin.html")
-});
-
-
